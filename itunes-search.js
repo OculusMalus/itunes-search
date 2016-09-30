e9c6a7dbed964f57a2275757b72a04bf4b61cfff
OculusMalus <stphn70@gmail.com>
2016-09-30 17:19:02 -0500

    pagination successful

--- itunes-search.js
+++ itunes-search.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $("#input").click(function(){
-            performSearch();
-            });
+        performSearch();
     });
-    
+});
+
 /*function handleCarousel(){
     $("#myCarousel").carousel({interval: false});
 
@@ -16,71 +16,91 @@ function paginateResults(){
 
 }
 
+
+
+
+
+
+
 function performSearch(){
 
     var terms = jQuery('#search-keyword').val()
 
     $.ajax({
-    "url": "https://itunes.apple.com/search?",
-    "type": "GET",
-    "dataType": "JSONP",
-    "data": {"term": terms },
-    "success": function(data) {handleTunesSearchResults(data)}
+
+
+        "url": "https://itunes.apple.com/search?",
+        "type": "GET",
+        "dataType": "JSONP",
+        "data": {"term": terms },
+        "success": function(data) {
+            $('#pagination-container').pagination({
+                dataSource: data.results,
+                callback: function(data, pagination) {
+                    var html = handleTunesSearchResults(data);
+                    $('#data-container').html(html);
+                }
+            })
+        }
     })
+
 }
 
 
-function handleTunesSearchResults(data) {
-    var results = data.results;
-    var html = '<h2>Results</h2>';
+
+    function handleTunesSearchResults(data) {
+        var results = data;
+        
+        var html = '<h2>Results</h2>';
 
 
 
-$.each(results, function(i, value){
+        $.each(results, function(i, value){
 
-    var item = results[i];
-    var object = {  
-                    artworkUrl100: item.artworkUrl100,
-                    track_name: item.trackCensoredName,
-                    track_id: item.trackId,
-                    previewUrl: item.previewUrl,
-                    track_url: item.trackViewUrl,
-                    trackPrice: item.trackPrice,
-                    artist_name: item.artistName,
-                    artist_url: item.artistViewUrl,
-                    collection_name: item.collectionCensoredName,
-                    collection_url: item.collectionViewUrl,
-                    genre: item.primaryGenreName
-                    };
+            var item = results[i];
+
+            var object = {  
+                artworkUrl100: item.artworkUrl100,
+                track_name: item.trackCensoredName,
+                track_id: item.trackId,
+                previewUrl: item.previewUrl,
+                track_url: item.trackViewUrl,
+                trackPrice: item.trackPrice,
+                artist_name: item.artistName,
+                artist_url: item.artistViewUrl,
+                collection_name: item.collectionCensoredName,
+                collection_url: item.collectionViewUrl,
+                genre: item.primaryGenreName
+            };
 
 
-    html +='<tr>'
-    html += '<td rowspan="2"><img class="img-responsive" src="{0}"></td>'.replace("{0}", object.artworkUrl100);
-    html += '<td>{0}</td>'.replace("{0}", object.track_name);                     
-    html += '<td><span class="label">Artist: </span><a href="{0}" target="_blank">{1}</a></td>'.replace("{0}", object.artist_url).replace("{1}", object.artist_name);
-    html += '<td><a href="{0}" target="_blank">Buy Song</a></td>'.replace("{0}", object.track_url);
+            html +='<tr>'
+            html += '<td rowspan="2"><img class="img-responsive" src="{0}"></td>'.replace("{0}", object.artworkUrl100);
+            html += '<td>{0}</td>'.replace("{0}", object.track_name);                     
+            html += '<td><span class="label">Artist: </span><a href="{0}" target="_blank">{1}</a></td>'.replace("{0}", object.artist_url).replace("{1}", object.artist_name);
+            html += '<td><a href="{0}" target="_blank">Buy Song</a></td>'.replace("{0}", object.track_url);
 
-        if (object.trackPrice < 0){ html += '<td align="right">N/A</td>';}
-        else {html += '<td><span class="label">Track Price: </span>${0} </td>'.replace("{0}", object.trackPrice)}    
+            if (object.trackPrice < 0){ html += '<td align="right">N/A</td>';}
+            else {html += '<td><span class="label">Track Price: </span>${0} </td>'.replace("{0}", object.trackPrice)}    
 
-    html +='</tr><tr><td><a href="{0}" target="_blank">Preview</a></td>'.replace("{0}", object.previewUrl);
-    html += '<td><span class="label">Collection:</span><a href="{0}" target="_blank">{1}</a></td>'.replace("{0}", object.collection_url).replace("{1}", object.collection_name);
+                html +='</tr><tr><td><a href="{0}" target="_blank">Preview</a></td>'.replace("{0}", object.previewUrl);
+            html += '<td><span class="label">Collection:</span><a href="{0}" target="_blank">{1}</a></td>'.replace("{0}", object.collection_url).replace("{1}", object.collection_name);
 
-        if (item.collectionPrice < 0){ html += '<td align="right">N/A</td>';}
-        else {html += '<td><span class="label">Collection Price: </span>${0}</td>'.replace("{0}", item.collectionPrice);}    
-    
-    html += '<td><span class="label">Primary Genre:</span>{0}</td></tr><br>'.replace("{0}", object.genre);
-      
+            if (item.collectionPrice < 0){ html += '<td align="right">N/A</td>';}
+            else {html += '<td><span class="label">Collection Price: </span>${0}</td>'.replace("{0}", item.collectionPrice);}    
 
-    
-    /*LOAD CAROUSEL WITH THIS OBJECT or CALL SOME FUNCTION TO DO IT*/
-    
+            html += '<td><span class="label">Primary Genre:</span>{0}</td></tr><br>'.replace("{0}", object.genre);
+
+
+
+            /*LOAD CAROUSEL WITH THIS OBJECT or CALL SOME FUNCTION TO DO IT*/
+
     /*WAIT?? FOR SLIDE EVENT
     BACK TO BEGINNING OF LOOP*/
 
-    })
+})
 
-
-jQuery('#itunes-results').html(html)
+        return html;
+// jQuery('#itunes-results').html(html)
 
 }
